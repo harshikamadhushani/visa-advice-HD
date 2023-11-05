0bec719c7bde93645209c3b0fe9a09b56bcd1113
Royan Harsha <royan@konekt.lk>
2023-11-05 11:49:22 +0530

    Add : Environment Setup

---
+++ resources/views/layouts/head-css.blade.php
@@ -0,0 +1,17 @@
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="{{ URL::asset('/build/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ URL::asset('/build/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link href="{{ URL::asset('/build/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ URL::asset('/build/assets/css/soft-ui-dashboard.css') }}" rel="stylesheet" />
+  {{-- ?v=1.0.7 --}}
+  <!-- Nepcha Analytics (nepcha.com) -->
+  <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+  <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+
+
+  @yield('css')
