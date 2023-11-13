1ac326d7ad0ffc9cc6541cdb26207de0328dea5b
Harshika Madhushani <harshika.m@parallax.lk>
2023-11-13 22:59:18 +0530

    footer remove

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -23,7 +23,7 @@
         </div>
         <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
           <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-            <span class="text-lg fab fa-dribbble"></span>
+            <span class="text-lg fab fa-facebook"></span>
           </a>
           <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
             <span class="text-lg fab fa-twitter"></span>
@@ -32,20 +32,11 @@
             <span class="text-lg fab fa-instagram"></span>
           </a>
           <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
-            <span class="text-lg fab fa-pinterest"></span>
+            <span class="text-lg fab fa-linkedin"></span>
           </a>
           <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
             <span class="text-lg fab fa-github"></span>
           </a>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-8 mx-auto text-center mt-1">
-          <p class="mb-0 text-secondary">
-            Copyright © <script>
-              document.write(new Date().getFullYear())
-            </script> Soft by Creative Tim.
-          </p>
         </div>
       </div>
     </div>
